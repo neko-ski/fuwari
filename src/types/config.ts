5f462cdae03270cdd6541b711e673ea852fc8fd2
Neaki.Ski <85069226+neko-ski@users.noreply.github.com>
2026-09-25 11:39:21 +0000

    Remove demo title and post URL

--- src/types/config.ts
+++ src/types/config.ts
@@ -2,7 +2,7 @@ import type { AUTO_MODE, DARK_MODE, LIGHT_MODE } from "@constants/constants";
 
 export type SiteConfig = {
 	title: string;
-	subtitle: string;
+	subtitle?: string;
 
 	lang:
 		| "en"
